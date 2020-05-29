aacf1ca549bb6da4c6db953eeb2387bef03e06d0
刘彦杉 <liuyanshan@myhayo.com>
2020-05-29 12:58:45 +0800

    未定义value时不处理

--- resources/views/amap.blade.php
+++ resources/views/amap.blade.php
@@ -9,16 +9,16 @@
         <div class="row">
             <div class="col-sm-3 {{$hidden_lng_lat_input ? 'hidden' : ''}}">
                 <input id="{{$column['lng']}}" name="{{$name['lng']}}" class="form-control"
-                       value="{{ old($column['lng'], $value['lng']) }}" {!! $attributes !!} />
+                       value="{{ old($column['lng'], @$value['lng']) }}" {!! $attributes !!} />
             </div>
             <div class="col-sm-3 {{$hidden_lng_lat_input ? 'hidden' : ''}}">
                 <input id="{{$column['lat']}}" name="{{$name['lat']}}" class="form-control"
-                       value="{{ old($column['lat'], $value['lat']) }}" {!! $attributes !!} />
+                       value="{{ old($column['lat'], @$value['lat']) }}" {!! $attributes !!} />
             </div>
 
             <div class="{{$hidden_lng_lat_input ? 'col-sm-12' : 'col-sm-6'}}">
                 <div class="input-group">
-                    <input type="text" class="form-control" id="{{$column['address']}}" name="{{$column['address']}}" value="{{$value['address']}}">
+                    <input type="text" class="form-control" id="{{$column['address']}}" name="{{$column['address']}}" value="{{@$value['address']}}">
                     <span class="input-group-btn">
                         <button type="button" class="btn btn-info btn-flat"><i class="fa fa-search"></i></button>
                     </span>
